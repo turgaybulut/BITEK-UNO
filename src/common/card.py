b3cac7d302ffbdd9761a28b12ba9185d46c60b7c
Turgay Bulut <turgay.bulut@ozu.edu.tr>
2025-01-04 04:31:49 +0300

    Fix card validation

--- src/common/card.py
+++ src/common/card.py
@@ -33,15 +33,12 @@ class Card:
         if current_color:
             return self.color == current_color
 
-        return (
-            self.color == other.color
-            or (
-                self.type == other.type
-                and self.type == CardType.NUMBER
-                and self.value == other.value
-            )
-            or (self.type == other.type and self.type != CardType.NUMBER)
-        )
+        if self.type == other.type:
+            if self.type == CardType.NUMBER:
+                return self.value == other.value
+            return True
+
+        return False
 
     def get_score_value(self) -> int:
         if self.type == CardType.NUMBER:
