65b7f9e9874d41ea613cd0ecb4201a91c2515870
Turgay Bulut <turgay.bulut@ozu.edu.tr>
2025-01-05 14:14:21 +0300

    Clear init file

--- src/client/ui/__init__.py
+++
@@ -1,9 +0,0 @@
-from client.ui.game_ui import GameUI
-from client.ui.chat_box import ChatBox
-from client.ui.game_board import GameBoard
-from client.ui.game_controls import GameControls
-from client.ui.player_hand import PlayerHand
-
-__all__ = ["GameUI", "ChatBox", "GameBoard", "GameControls", "PlayerHand"]
-
-
